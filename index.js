4bffcb2e5b9cf38d708d8cdb8b19e77ed758e4db
kiyaiB <babra.kiyai@student.moringaschool.com>
2022-04-10 16:31:35 +0300

    Finished Asignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arr, msg){
+    let cardsMsg = [];
+    for(let item in arr){
+        cardsMsg.push(`Thank you, ${arr[item]}, for the wonderful ${msg} gift!`);
+    }
+    return cardsMsg;
+}
+
+function countDown(no){
+    let x = 0;
+    while(x < no + 1){
+        console.log(no - x)
+        x++
+    }
+    
+}
